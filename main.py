9b32c818849f2d3b1dec96d1a6d6919d2dec8fd3
Oscar Hogben <83372155+Oscar-Hogben@users.noreply.github.com>
2021-06-23 14:42:26 +0000

    Added a text colour class and a welcome message

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 
 emptySpace = "-"
 
+class bcolours:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def gridReset():
     grid = [
         [emptySpace, emptySpace, emptySpace],
@@ -48,7 +59,7 @@ def checkWin():
     # No one has won
     return False
 
-
+print(bcolours.BOLD + "Welcome to Noughts and Crosses!" + bcolours.ENDC)
 
 crossGo = False
 
